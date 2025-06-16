a419a0d236b3d96e03098f857645170a867bc92b
Anish Timsina <anishtimsina899@gmail.com>
2025-06-16 08:47:35 +0545

    Practicing Terraform

--- Setting_Terraform_for_AWS/main.tf
+++ Setting_Terraform_for_AWS/main.tf
@@ -1,10 +1,10 @@
-provider "aws" {
-	region = "us-east-1" 
+#provider "aws" {
+#	region = "us-east-1" 
 #set desired region
-}
+#}
 
-resource "aws_instance" "example" {
-	ami = "ami-0c55b159cbfafe1f0"
+#resource "aws_instance" "example" {
+#	ami = "ami-0c55b159cbfafe1f0"
 #spoecify appropriate id
-	instance_type = "t2.micro"
-}
+#	instance_type = "t2.micro"
+#}
